4031106ba93b1f157586a907843e46f89bb6ae29
Brian Hays <brianhays@Brians-MacBook-Pro.local>
2017-03-24 10:54:17 -0600

    server runs

---
+++ server.js
@@ -0,0 +1,11 @@
+
+require('colors');
+var express    = require('express'),
+    mongoose   = require('mongoose'),
+    PORT       = 3000;
+
+var app = express();
+
+app. server = app.listen(PORT, ()=>{
+  console.log(`Server running on ${PORT}`.america);
+});
